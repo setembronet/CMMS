0823c1e4b42f17372cddbd048ba3337e16e58ba3
Alexandre Oliveira <setembronet@gmail.com>
2025-10-11 00:10:46 +0000

    Nao está dando certo. Tente outra abordagem mais eficiente

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,6 +1,7 @@
 
 'use client';
 
+import * as React from 'react';
 import {
   Sidebar,
   SidebarProvider,
@@ -11,27 +12,6 @@ import { SidebarNav } from '@/components/dashboard/sidebar-nav';
 import { I18nProvider } from '@/context/i18n-provider';
 import { ClientProvider, useClient } from '@/context/client-provider';
 
-function DashboardLayoutContent({ children }: { children: React.ReactNode }) {
-  const { currentUser } = useClient();
-
-  if (!currentUser) {
-    // Or a loading spinner, or some other placeholder
-    return null;
-  }
-  
-  return (
-    <SidebarProvider>
-      <Sidebar>
-        <SidebarNav />
-      </Sidebar>
-      <SidebarInset>
-        <Header />
-        <main className="p-4 sm:p-6 lg:p-8 flex-1 overflow-y-auto">{children}</main>
-      </SidebarInset>
-    </SidebarProvider>
-  );
-}
-
 
 export default function DashboardLayout({
   children,
@@ -41,8 +21,17 @@ export default function DashboardLayout({
   return (
     <I18nProvider>
       <ClientProvider>
-        <DashboardLayoutContent>{children}</DashboardLayoutContent>
+        <SidebarProvider>
+          <Sidebar>
+            <SidebarNav />
+          </Sidebar>
+          <SidebarInset>
+            <Header />
+            <main className="p-4 sm:p-6 lg:p-8 flex-1 overflow-y-auto">{children}</main>
+          </SidebarInset>
+        </SidebarProvider>
       </ClientProvider>
     </I18nProvider>
   );
 }
+
