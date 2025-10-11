58830f0bbe53f22abd9d1377ff241cb68093738f
Alexandre Oliveira <setembronet@gmail.com>
2025-10-11 00:27:30 +0000

    Siga estas dicas Essa é uma situação frustrante, mas se **nenhum** dos p

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type {Metadata} from 'next';
-import '@/app/globals.css';
+import '@/globals.css';
 import { cn } from '@/lib/utils';
 import { Toaster } from '@/components/ui/toaster';
 import { FirebaseClientProvider } from '@/firebase/client-provider';
