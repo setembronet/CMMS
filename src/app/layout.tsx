f4381878f196d22bd3ff6e5e656065c281949cdd
Alexandre Oliveira <setembronet@gmail.com>
2025-10-16 19:39:02 +0000

    teria como isola-lo para testarmos se vem dele o erro

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type {Metadata} from 'next';
 import './globals.css';
 import { cn } from '@/lib/utils';
 import { Toaster } from '@/components/ui/toaster';
-import { FirebaseClientProvider } from '@/firebase/client-provider';
+// import { FirebaseClientProvider } from '@/firebase/client-provider';
 
 export const metadata: Metadata = {
   title: 'TenantCare CMMS',
@@ -22,9 +22,9 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Space+Grotesk:wght@500;600;700&display=swap" rel="stylesheet" />
       </head>
       <body className={cn("font-body antialiased", "min-h-screen bg-background font-sans")}>
-          <FirebaseClientProvider>
+          {/* <FirebaseClientProvider> */}
             {children}
-          </FirebaseClientProvider>
+          {/* </FirebaseClientProvider> */}
           <Toaster />
       </body>
     </html>
