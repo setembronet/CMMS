e230890e2e31eea54d96c1db919acd6b42b12706
Alexandre Oliveira <setembronet@gmail.com>
2025-10-21 18:50:52 +0000

    **INSTRUÇÃO CRÍTICA: RESOLUÇÃO DE CONFLITO DE DIRETÓRIOS E ERRO 404**
    M

---
+++ next.config.js
@@ -0,0 +1,36 @@
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  srcDir: 'src',
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+       {
+        protocol: 'https',
+        hostname: 'api.qrserver.com',
+        port: '',
+        pathname: '/**',
+      }
+    ],
+  },
+};
+
+module.exports = nextConfig;
