eb88b54df207a9342b595bc282353370d10694af
Alexandre Oliveira <setembronet@gmail.com>
2025-10-22 13:21:38 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve '@/

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,4 @@
+
 import type {Config} from 'tailwindcss';
 
 export default {
@@ -6,6 +7,7 @@ export default {
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
